17e18e8a20ad21c77bf088830f390fc4544e4500
Matt Teichman <teichman@uchicago.edu>
2025-04-16 14:39:17 -0500

    adding some comments

--- lib/lib.ml
+++ lib/lib.ml
@@ -1,58 +1,56 @@
+module Xchg = struct
+  open Effect
+  type _ Effect.t += Xchg : int -> int t
+
+  (* from the Effect module: *)
+  (* type 'a t = 'a eff = .. *)
+  (* external perform : 'a t -> 'a = "%perform" *)
+
+  (* unit -> int *)
+  let comp' () = perform (Xchg 0) + perform (Xchg 1)
+end
+
 module WhyNotJustDoThis = struct
   type t =
     | Xchg of int
 
+  (* t -> int *)
   let handle = function
     | Xchg n -> n + 1
-            
+
+  (* unit -> int *)
   let comp' () = handle (Xchg 0) + handle (Xchg 1)
+
+  (* answer: you don't get the k *)
 end
 
 module Simple = struct
-  open Effect
+  include Xchg
   open Effect.Deep
 
-  type _ Effect.t += Xchg : int -> int t
-  let comp' () = perform (Xchg 0) + perform (Xchg 1)
+  (* unit -> int *)
   let comp () =
     try comp' () with
     | effect (Xchg n), k -> continue k (n + 1)
 
+  (* unit -> int *)
   let double_k_comp () =
     try comp' () with
     | effect (Xchg n), k -> continue k (n + 1) + continue k (n - 1)
 
+  (* unit -> int *)
   let no_k_comp () =
     try comp' () with
     | effect (Xchg n), _ -> n + 1
 end
 
-module StringSimple = struct
-  open Effect
+module MessagePassing = struct
+  include Xchg
   open Effect.Deep
 
-  type _ Effect.t += Xchg : string -> string t
-  let comp' () = perform (Xchg "hi") ^ perform (Xchg "keith")
-  let comp () =
-    try comp' () with
-    | effect (Xchg n), k ->
-       print_endline "AHAOHAOI" ;
-       continue k (n ^ " stuff")
+  type 'a status =
+    | Complete of 'a
+    | Suspended of { msg : int
+                   ; cont : (int, 'a status) continuation }
 
-  let no_k_comp () =
-    try comp' () with
-    | effect (Xchg n), _ ->
-       print_endline "OIHFOEWHFIEWOHF" ;
-       n ^ " things"
-
-  let let_comp' () =
-    let x = perform (Xchg "hi") in
-    let y = perform (Xchg "keith") in
-    x ^ y
-
-  let handler effectful =
-    try effectful () with
-    | effect (Xchg n), _ ->
-       print_endline "OIHFOEWHFIEWOHF" ;
-       n ^ " things"
 end
