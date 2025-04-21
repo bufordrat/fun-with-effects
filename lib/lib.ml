301373c9ba44fff1530dff858faf640791e17e31
Matt Teichman <teichman@uchicago.edu>
2025-04-21 14:36:20 -0500

    finally have the scheduler example

--- lib/lib.ml
+++ lib/lib.ml
@@ -60,7 +60,7 @@ module MessagePassing = struct
     match f () with
     | v -> Complete v
     | effect (Xchg msg), cont ->
-       print_endline (string_of_int msg) ;
+       (* print_endline (string_of_int msg) ; *)
        Suspended { msg ; cont }
 
   let rec run_both a b =
@@ -103,12 +103,88 @@ module MessagePassing = struct
          (fun () -> continue k1 m2)
          (fun () -> continue k2 m1)
     | _ -> failwith "Improper synchronization"
-
-
 end
 
 module Example = struct
   open Effect
+  open Effect.Deep
   type _ Effect.t += Wzap : int -> int t
+
+  let left_wzap () = perform (Wzap 2) + 3
+  let right_wzap () = 3 + perform (Wzap 2)
+
+  let run effects () =
+    match effects () with
+    | effect (Wzap n), k -> continue k n
+    | other -> other
   
+  let run' effects () =
+    match effects () with
+    | effect (Wzap n), _ -> n
+    | other -> other
+end
+
+module Scheduler = struct
+  open Effect
+  open Effect.Deep
+  type _ Effect.t +=
+     | Xchg : int -> int t
+     | Fork : (unit -> unit) -> unit t
+     | Yield : unit t
+
+  let fork f = perform (Fork f)
+  let yield () = perform Yield
+  let xchg v = perform (Xchg v)
+
+  let run main =
+    let exchanger
+        : (int * (int, unit) continuation) option ref
+      = ref None
+    in
+    let run_q = Queue.create () in
+    let enqueue k v =
+      let task () = continue k v in
+      Queue.push task run_q
+    in
+    let dequeue () =
+      if Queue.is_empty run_q
+      then ()
+      else 
+        let task = Queue.pop run_q in
+        task ()
+    in
+    let rec spawn f =
+      match f () with
+      | () -> dequeue ()
+      | exception e ->
+         print_endline (Printexc.to_string e) ;
+         dequeue ()
+      | effect Yield, k -> enqueue k () ;
+                           dequeue ()
+      | effect (Fork f), k -> enqueue k () ;
+                              spawn f
+      | effect (Xchg n), k ->
+         begin
+           match !exchanger with
+           | Some (n', k') -> exchanger := None ;
+                              enqueue k' n ;
+                              continue k n'
+           | None -> exchanger := Some (n, k) ;
+                     dequeue ()
+         end
+    in
+    spawn main
+
+  let doit _ =
+    run
+      (fun _ ->
+        fork (fun _ ->
+            Printf.printf "[t1] Sending 0\n" ;
+            let v = xchg 0 in
+            Printf.printf "[t1] received %d\n" v ;) ;
+        fork (fun _ ->
+            Printf.printf "[t2] Sending 1\n" ;
+            let v = xchg 1 in
+            Printf.printf "[t2] received %d\n" v ;) ;
+      )
 end
