5bed94dc7e376af92b7aa857cd6933235d604f15
Matt Teichman <teichman@uchicago.edu>
2025-04-25 11:49:46 -0500

    ok, examples are semi-finalized

---
+++ lib/round_robin.ml
@@ -0,0 +1,62 @@
+open Effect
+type _ Effect.t +=
+   | Xchg : int -> int t
+   | Yield : unit t
+             | Fork : (unit -> unit) -> unit t
+
+let fork f = perform (Fork f)
+let yield () = perform Yield
+let xchg n = perform (Xchg n)
+
+open Effect.Deep
+
+let scheduler main =
+  let exchanger
+      : (int * (int, unit) continuation) option ref
+    = ref None
+  in
+  let run_q = Queue.create () in
+  let enqueue k v =
+    let task () = continue k v in
+    Queue.push task run_q
+  in
+  let dequeue () =
+    if Queue.is_empty run_q
+    then ()
+    else let task = Queue.pop run_q in
+         task ()
+  in
+  let rec spawn f =
+    match f () with
+    | () -> dequeue ()
+    | exception e ->
+       print_endline (Printexc.to_string e) ;
+       dequeue ()
+    | effect Yield, k ->
+       enqueue k () ;
+       dequeue ()
+    | effect (Fork f), k ->
+       enqueue k () ;
+       spawn f
+    | effect (Xchg n), k ->
+       match !exchanger with
+       | Some (n', k') -> exchanger := None ;
+                          enqueue k' n ;
+                          continue k n'
+       | None -> exchanger := Some (n, k) ;
+                 dequeue ()
+  in
+  spawn main
+
+let doit () =
+  let print1 _ =
+    Printf.printf "[t1] Sending 0\n" ;
+    let v = xchg 0 in
+    Printf.printf "[t1] received %d\n" v
+  in
+  let print2 _ =
+    Printf.printf "[t2] Sending 1\n" ;
+    let v = xchg 1 in
+    Printf.printf "[t2] received %d\n" v
+  in
+  scheduler (fun _ -> fork print1 ; fork print2)
---
+++ lib/swap.ml
@@ -0,0 +1,36 @@
+open Effect
+
+type _ Effect.t +=
+   | Swap : int -> int t
+
+open Effect.Deep
+type 'a status =
+  | Complete of 'a
+  | Suspended of { number : int ;
+                   cont : (int, 'a status) continuation ; }
+
+let freeze_effect computation () =
+  match computation () with
+  | effect (Swap n), k -> Suspended { number = n ; cont = k }
+  | n -> Complete n
+
+let rec scheduler comp1 comp2 =
+  match comp1 (), comp2 () with
+  | Complete x, Complete y -> x, y
+  | Suspended { number = number1 ;
+                cont = cont1 ; },
+    Suspended { number = number2 ;
+                cont = cont2 ; }
+    -> scheduler
+         (fun _ -> continue cont1 number2)
+         (fun _ -> continue cont2 number1)
+  | _ -> failwith "synchronization error"
+
+let comp1 () =
+  perform (Swap 0) + perform (Swap 1)
+
+let comp2 () =
+  perform (Swap 21) * perform (Swap 21)
+
+let doit () =
+  scheduler (freeze_effect comp1) (freeze_effect comp2)
